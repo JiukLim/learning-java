b258ce99fc2a62c2e690179eda74d75ec4bbacdf
jiuk.lim <jiuk.lim@navercorp.com>
2020-12-06 22:26:29 +0900

    initial commit

---
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class HelloWorldController {
+
+    @GetMapping("/hello-world")
+    public String helloWorld() {
+        return "hello world";
+    }
+}
